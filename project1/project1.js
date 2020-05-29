4e66428c1149a27bee8dc02c8bd7f6a0b56078ea
Eugene <ndrvevg@gmail.com>
2020-05-29 19:59:36 +0300

    29.05.20

--- project1/project1.js
+++ project1/project1.js
@@ -1,29 +1,29 @@
 'use strict';
-let money;
-let time;
-let expences = {};
-let optionalExpences = {};
+let money = prompt ("Ваш бюджет на месяц?");
+let time = prompt ("Введите дату в формате YYYY-MM-DD");
+let expenses = {};
+let optionalExpences ={};
 let income = [];
-money = prompt ("Ваш бюджет на месяц?");
-time = prompt ("Введите дату в формате YYYY-MM-DD");
-let appData = {
-    budget: money,
-    timeData: time,
-    expences,
-    optionalExpences,
-    income,
-    savings:false,
-};
 
 let budjetEntry1 = prompt ("Введите обязательную строку расходов в этом месяце");
 let exp1 = prompt ("Во сколько обойдется?");
 let budjetEntry2 = prompt ("Введите обязательную строку расходов в этом месяце");
 let exp2 = prompt ("Во сколько обойдется?");
-expences: {
-    budjetEntry1:exp1
+
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses : {
+        budjetEntry1 : exp1,  //если нужно определить имя свойства - писать в квадратгых скобках вычисляемые свойства
+        budjetEntry2 : exp2,  //https://learn.javascript.ru/object - взять имя свойства из перепменной 
+    },
+    optionalExpences,
+    income,
+    savings:false,
 };
-expences: {
-    budjetEntry2:exp2
-    };
-let dayBudjet = (money - exp1 - exp2) / 30;  // - expences[budjetEntry1]-expences[budjetEntry2];//=money-expences;
-alert (dayBudjet);
+
+
+
+
+
+console.log (expenses.budjetEntry1);
